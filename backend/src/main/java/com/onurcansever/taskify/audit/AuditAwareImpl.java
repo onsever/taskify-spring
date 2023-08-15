9242b6de47826aac4659b1417858927f72051c58
Onurcan Sever <onsever@icloud.com>
2023-08-15 19:53:19 -0400

    initialize spring boot application and implement base entity with audit support for further use

---
+++ backend/src/main/java/com/onurcansever/taskify/audit/AuditAwareImpl.java
@@ -0,0 +1,16 @@
+package com.onurcansever.taskify.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component(value = "auditAwareImpl")
+public final class AuditAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
+}
