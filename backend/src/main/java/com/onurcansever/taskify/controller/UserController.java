badb95ee90e42bb56e298345643ebee52e5c548d
Onurcan Sever <onsever@icloud.com>
2023-08-16 11:24:45 -0400

    implement rest api endpoints for user resource

--- backend/src/main/java/com/onurcansever/taskify/controller/UserController.java
+++ backend/src/main/java/com/onurcansever/taskify/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.onurcansever.taskify.controller;
 
+import com.onurcansever.taskify.dto.UserDto;
 import com.onurcansever.taskify.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -14,5 +16,22 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
+        return new ResponseEntity<>(this.userService.getUserById(id), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable Long id) {
+        return new ResponseEntity<>(this.userService.updateUser(userDto, id), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<String> deleteUserById(@PathVariable Long id) {
+        this.userService.deleteUserById(id);
+
+        return new ResponseEntity<>(String.format("User with id %d has been deleted successfully!", id), HttpStatus.NO_CONTENT);
     }
 }
