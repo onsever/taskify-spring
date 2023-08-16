28927b12ab5533db9c7af45fb90ff4f97f4ccaad
Onurcan Sever <onsever@icloud.com>
2023-08-16 18:15:32 -0400

    add repository annotations on class level

--- backend/src/main/java/com/onurcansever/taskify/repository/TaskRepository.java
+++ backend/src/main/java/com/onurcansever/taskify/repository/TaskRepository.java
@@ -2,7 +2,9 @@ package com.onurcansever.taskify.repository;
 
 import com.onurcansever.taskify.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
 }
