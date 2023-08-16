d3739cb002456c877ebfd4eb6ddc9fcc704ca479
Onurcan Sever <onsever@icloud.com>
2023-08-16 00:15:13 -0400

    add annotations for enable jpa and entity configurations

--- backend/src/main/java/com/onurcansever/taskify/TaskifyApplication.java
+++ backend/src/main/java/com/onurcansever/taskify/TaskifyApplication.java
@@ -2,10 +2,14 @@ package com.onurcansever.taskify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EntityScan("com.onurcansever.taskify.entity")
+@EnableJpaRepositories("com.onurcansever.taskify.repository")
 public class TaskifyApplication {
 
 	public static void main(String[] args) {
