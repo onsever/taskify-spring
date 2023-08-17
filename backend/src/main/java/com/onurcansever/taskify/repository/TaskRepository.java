e60d1db52d08e6dd0ce71582442c401c2c002e0e
Onurcan Sever <onsever@icloud.com>
2023-08-16 20:03:28 -0400

    implement custom sql query to retrieve all tags associated with the task id

--- backend/src/main/java/com/onurcansever/taskify/repository/TaskRepository.java
+++ backend/src/main/java/com/onurcansever/taskify/repository/TaskRepository.java
@@ -1,10 +1,16 @@
 package com.onurcansever.taskify.repository;
 
+import com.onurcansever.taskify.entity.Tag;
 import com.onurcansever.taskify.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Set;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
+    @Query("SELECT t.tags FROM Task t WHERE t.taskId = :taskId")
+    Set<Tag> findTagsByTaskId(Long taskId);
 }
