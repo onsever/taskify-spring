2b2879822809f6220ae3fad828878f80e8ea40f4
Onurcan Sever <onsever@icloud.com>
2023-08-18 15:21:02 -0400

    create global file to include all types that will be used in the application

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,35 @@
+type User = {
+  userId: number;
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+};
+
+type Task = {
+  taskId: number;
+  title: string;
+  description: string;
+  status: string;
+  dueDate: string;
+};
+
+type JWT = {
+  accessToken: string;
+  tokenType: string;
+};
+
+type LoginCredentials = {
+  usernameOrEmail: string;
+  password: string;
+};
+
+type RegisterCredentials = {
+  username: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+};
+
+export type { User, Task, JWT, LoginCredentials, RegisterCredentials };
