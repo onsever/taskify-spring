7c5fac64d09ab3f2c3ed670eb2b8d7f163df3744
Onurcan Sever <onsever@icloud.com>
2023-08-18 15:19:53 -0400

    test query method

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,14 @@
 import { Container, Heading, Spacer } from "./styles.ts";
 import { Board } from "../../components";
+import { useGetUserByIdQuery } from "../../redux/features/usersFeature.ts";
 
 export default function Dashboard() {
+  const { data, isLoading, isError } = useGetUserByIdQuery(1);
+
+  console.log(import.meta.env);
+
+  console.log(data, isLoading, isError);
+
   return (
     <Container>
       <Spacer />
