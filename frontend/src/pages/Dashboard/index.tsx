f93d41e97625c4bb9a38e7f7e976517c466b08b5
Onurcan Sever <onsever@icloud.com>
2023-08-20 21:52:15 -0400

    add refetch functionality

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,9 +1,14 @@
 import { Container, Heading, Spacer } from "./styles.ts";
 import { Board } from "../../components";
 import { useGetTasksQuery } from "../../redux/features/task/taskFeature.ts";
+import React from "react";
 
 export default function Dashboard() {
-  const { data, isLoading } = useGetTasksQuery();
+  const { data, isLoading, refetch } = useGetTasksQuery();
+
+  React.useEffect(() => {
+    refetch();
+  }, [refetch]);
 
   if (isLoading) {
     return <h1>Loading...</h1>;
