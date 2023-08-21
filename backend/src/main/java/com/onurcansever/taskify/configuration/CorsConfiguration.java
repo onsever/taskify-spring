ab45a8d346b713caff970fb4a139fbdb53b24d4e
Onurcan Sever <onsever@icloud.com>
2023-08-20 22:28:35 -0400

    fix cors bug

--- backend/src/main/java/com/onurcansever/taskify/configuration/CorsConfiguration.java
+++ backend/src/main/java/com/onurcansever/taskify/configuration/CorsConfiguration.java
@@ -11,10 +11,10 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(false)
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
