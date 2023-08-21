393b98d5f8c828d7341a7f33b70ebccdd90fddbd
Onurcan Sever <onsever@icloud.com>
2023-08-21 11:09:23 -0400

    create filter option component for filtering the tasks

---
+++ frontend/src/components/FilterTabs/styles.ts
@@ -0,0 +1,13 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+  margin-top: 0.8rem;
+  padding-left: 2rem;
+`;
+
+export const Select = styled.select`
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 0.25rem;
+  margin-bottom: 1rem;
+`;
